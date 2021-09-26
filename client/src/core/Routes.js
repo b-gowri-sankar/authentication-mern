1f63f1d685db55e817a6c98a62659f44b9618c3f
Gowri Sankar <sankaryadavrgukt@gmail.com>
2021-09-26 19:04:22 +0530

    private route

--- client/src/core/Routes.js
+++ client/src/core/Routes.js
@@ -4,6 +4,8 @@ import App from "../App";
 import SignIn from "../auth/SignIn";
 import Signup from "../auth/Signup";
 import Activate from "../auth/Activate";
+import Private from "./Private";
+import PrivateRoute from "../auth/PrivateRoute";
 
 const Routes = () => {
 	return (
@@ -13,6 +15,7 @@ const Routes = () => {
 				<Route path="/signup" exact component={Signup} />
 				<Route path="/signin" exact component={SignIn} />
 				<Route path="/auth/activate/:token" exact component={Activate} />
+				<PrivateRoute path="/private" exact component={Private} />
 			</Switch>
 		</BrowserRouter>
 	);
